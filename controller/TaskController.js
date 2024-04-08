26c5874e1411d4a9bece91c0c4b91c5c8a12e1bb
NicoliLuz <azevedonicoligithub@gmail.com>
2024-04-08 09:35:48 -0300

    meu primeiro commit

---
+++ controller/TaskController.js
@@ -0,0 +1,15 @@
+const database = require('../database/connection')
+
+class TaskController {
+    createTask(req, res) {
+        const {title, description, status} = req.body
+        database.insert({title, description, status}).table("task").then(function(data) {
+            res.json({message: 'Task created successfully'})
+        }).catch(function(error) {
+            res.json({error: error})
+            console.log(error)
+        })
+    }
+}
+
+module.exports = new TaskController()
